56b1cec85f92e02245bf0bdc2ae2aa8f9306ad50
Jarosław Sobczyński <jsobczynski94@gmail.com>
2016-02-08 17:59:04 +0100

    Init commit

---
+++ TSST.Agile.Web/App_Start/Startup.Auth.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TSST.Agile.Web.Security;
+
+namespace TSST.Agile.Web
+{
+    public partial class Startup
+    {
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            var OAuthserverOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                Provider = new AuthTokenProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthserverOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
---
+++ TSST.Agile.Web/Security/AuthTokenProvider.cs
@@ -0,0 +1,43 @@
+﻿using Facebook;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TSST.Agile.Web.Security
+{
+    public class AuthTokenProvider: OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantCustomExtension(OAuthGrantCustomExtensionContext context)
+        {
+            if(context.GrantType.ToLower() == "facebook")
+            {
+                var fbClient = new FacebookClient(context.Parameters.Get("accesstoken"));
+                dynamic mainDataResponse = await fbClient.GetTaskAsync("me", new { fields = "first_name, last_name, picture" });
+                dynamic friendListResponse = await fbClient.GetTaskAsync("me/friends");
+                string id = mainDataResponse.id;
+                string firstName = mainDataResponse.first_name;
+                string lastName = mainDataResponse.last_name;
+                string pictureUrl = mainDataResponse.picture.data.url;
+                //create user here
+                
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Name, firstName));
+                identity.AddClaim(new Claim(ClaimTypes.Surname, lastName));
+                identity.AddClaim(new Claim("id", id));
+
+                await base.GrantCustomExtension(context);
+                context.Validated(identity);
+            }
+            return;
+        }
+    }
+}
